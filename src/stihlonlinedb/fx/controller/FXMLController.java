4a56c2e5e7593c96d19be000f08702737acc559f
micki <mickivogel@gmail.com>
2017-09-19 15:49:13 +0200

    update

--- src/stihlonlinedb/fx/controller/FXMLController.java
+++ src/stihlonlinedb/fx/controller/FXMLController.java
@@ -1,21 +1,34 @@
 package stihlonlinedb.fx.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import stihlonlinedb.dao.queries.ListDbObjects;
+import stihlonlinedb.entities.Produkte;
 
 public class FXMLController implements Initializable {
 	@FXML
 	private HBox productHBox;
 	@FXML
 	private Label label;
+	@FXML
+	private FlowPane productPane;
 
-	public FXMLController() {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		addProductPane();
+		addProductFlowPaneContent();
+		addLabel();
 	}
 
 	private void addLabel() {
@@ -28,9 +41,24 @@ public class FXMLController implements Initializable {
 		productHBox.setVisible(true);
 	}
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		addProductPane();
-		addLabel();
+	private void addProductFlowPaneContent() {
+		productPane.setPadding(new Insets(10, 10, 10, 10));
+		productPane.setVgap(4);
+		productPane.setHgap(4);
+		productPane.setPrefWrapLength(170);
+
+		ListDbObjects dbObjects = new ListDbObjects();
+		List<Produkte> allProdukte = dbObjects.getAllProdukte();
+		ImageView views[] = new ImageView[allProdukte.size()];
+
+		for (Produkte produkte : allProdukte) {
+			Image image = new Image(getClass().getResourceAsStream(produkte.getBild().getPfad()), 120, 100, true, true);
+			ImageView view = new ImageView(image);
+			Button btn = new Button("", view);
+			productPane.getChildren().add(btn);
+		}
+		for (int i = 0; i < 8; i++) {
+		}
+
 	}
 }
